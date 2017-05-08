ec52e09863e6a3d30fb0335abb5842bb2749a49c
Max <maxsizen@gmail.com>
2017-05-08 17:12:16 +0400

    Add files via upload

---
+++ Assets/Script/Game/StateMachine/Input/EmptyInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThreeK.Game.StateMachine.Input;
+
+namespace ThreeK.Game.StateMachine.Input
+{
+    public class EmptyInput : GameInput
+    {
+        public EmptyInput(object data) : base(data)
+        {
+        }
+    }
+}
