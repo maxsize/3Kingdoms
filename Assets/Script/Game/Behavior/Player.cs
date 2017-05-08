c97ee4822dd994eea59bd7552aab95dbc72c7ef0
Max <maxsizen@gmail.com>
2017-05-08 17:14:31 +0400

    Add files via upload

--- Assets/Script/Game/Behavior/Player.cs
+++ Assets/Script/Game/Behavior/Player.cs
@@ -1,29 +1,50 @@
-﻿using Adic;
+﻿using System.Collections.Generic;
+using Adic;
 using Adic.Container;
-using System.Collections;
-using System.Collections.Generic;
 using ThreeK.Game.StateMachine;
 using ThreeK.Game.StateMachine.Input;
+using ThreeK.Game.StateMachine.State;
 using UnityEngine;
 
-public class Player : InjectableBehaviour
+public class Player : PushdownAutomation
 {
-    [Inject] public IMonoStateMachine Machine;
     [Inject] public IInjectionContainer Container;
 
-    // Use this for initialization
     protected override void Start()
     {
         base.Start();
+        PostConstruct();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown(KeyCode.Mouse0))
+
+    void PostConstruct()
+    {
+        var context = FindObjectOfType<ContextRoot>();
+        var subContainer = context.AddContainer<InjectionContainer>();
+        subContainer.RegisterExtension<UnityBindingContainerExtension>();
+        Container.Bind<IInjectionContainer>().To(subContainer).As("SubContainer");
+
+        subContainer.Bind<IStateMachine>().To(this)
+            .Bind<IState>().To<IdleState>().As(typeof(IdleState))
+            .Bind<IState>().To<MoveState>().As(typeof(MoveState))
+            .Bind<IState>().To<AttackState>().As(typeof(AttackState));
+
+
+        List<IState> states = new List<IState>
         {
-            var input = Container.Resolve<MoveInput>();
+            subContainer.Resolve<IState>(typeof(IdleState)),
+            subContainer.Resolve<IState>(typeof(MoveState)),
+            subContainer.Resolve<IState>(typeof(AttackState))
+        };
+        AddStates(states.ToArray(), states[0]);
+    }
+
+    // Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+		    var input = Container.Resolve<IInput>(typeof(MoveInput));
             if (input != null)
-                Machine.HandleInput(input);
+                HandleInput(input);
         }
 	}
 }
