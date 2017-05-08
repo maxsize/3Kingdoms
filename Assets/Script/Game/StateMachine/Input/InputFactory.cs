e0e33bbdf8a029153d9f5d1b16bdcbcef37e8e94
Max <maxsizen@gmail.com>
2017-05-08 17:11:16 +0400

    Update InputFactory.cs

--- Assets/Script/Game/StateMachine/Input/InputFactory.cs
+++ Assets/Script/Game/StateMachine/Input/InputFactory.cs
@@ -1,35 +1,37 @@
-﻿using Adic;
+using Adic;
 using Adic.Container;
 using Adic.Injection;
-using System;
 using ThreeK.Game.StateMachine.Input;
 using UnityEngine;
 
-public class InputFactory : IFactory
+namespace ThreeK.Game.StateMachine.Input
 {
-    [Inject] public IInjectionContainer Container;
-
-    public object Create(InjectionContext context)
+    public class InputFactory : IFactory
     {
-        if (context.injectType == typeof(MoveInput))
-            return CreateMoveInput(context);
-        return null;
-    }
+        [Inject] public IInjectionContainer Container;
 
-    private IInput CreateMoveInput(InjectionContext context)
-    {
-        var camera = Camera.main;
-        var fwd = camera.transform.TransformDirection(Vector3.forward);
-        float dist = 0.0f;
-        RaycastHit hit;
-        bool hitted = Physics.Raycast(camera.transform.position, fwd, out hit);
-        if (!hitted)
+        public object Create(InjectionContext context)
+        {
+            if (context.identifier.Equals(typeof(MoveInput)))
+                return CreateMoveInput(context);
             return null;
-        dist = Vector3.Distance(camera.transform.position, hit.point);
-        var mousePos = UnityEngine.Input.mousePosition;
-        mousePos.z = dist;
-        //_target = new Vector3(277.3f, 122.4f, 65.1f);
-        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-        return new MoveInput(mousePos);
+        }
+
+        private IInput CreateMoveInput(InjectionContext context)
+        {
+            var camera = Camera.main;
+            var fwd = camera.transform.TransformDirection(Vector3.forward);
+            float dist = 0.0f;
+            RaycastHit hit;
+            bool hitted = Physics.Raycast(camera.transform.position, fwd, out hit);
+            if (!hitted)
+                return null;
+            dist = Vector3.Distance(camera.transform.position, hit.point);
+            var mousePos = UnityEngine.Input.mousePosition;
+            mousePos.z = dist;
+            //_target = new Vector3(277.3f, 122.4f, 65.1f);
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            return new MoveInput(mousePos);
+        }
     }
 }
