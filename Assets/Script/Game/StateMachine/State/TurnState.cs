a19067daf19ca12fa0b2adc21c49278b85841227
Max <maxsizen@gmail.com>
2017-05-14 00:00:56 +0400

    refactor

--- Assets/Script/Game/StateMachine/State/TurnState.cs
+++ Assets/Script/Game/StateMachine/State/TurnState.cs
@@ -6,41 +6,36 @@ using ThreeK.Game.StateMachine.State;
 using Adic;
 using Adic.Container;
 using ThreeK.Game.StateMachine;
+using ThreeK.Game.Helper;
 
 namespace ThreeK.Game.StateMachine.State
 {
-    public class TurnState : MonoState
+    public class TurnState : State
     {
         [Inject] public IInjectionContainer Container;
 
-        private float _turnSpeed = 400;
         private Vector3 _target;
+        private Quaternion _data;
 
-        public TurnState(IStateMachine machine) : base(machine as MonoBehaviour)
+        public TurnState(IStateMachine stateMachine)
         {
+            _target = GetTarget(InputHelper.CurrentInput);
+            var rigi = (stateMachine as MonoBehaviour).GetComponent<Rigidbody>();
+            _data = Quaternion.LookRotation(_target - rigi.position);
+        }
+
+        public override object Data
+        {
+            get { return _data; }
         }
 
         public override IState HandleInput(IInput input)
         {
             if (input is AttackInput || input is MoveInput)
             {
-                Machine.StopCoroutine("TurnTowards");
-                OnStateExit.Invoke();
                 return Container.Resolve<IState>(typeof(StackedState));
             }
             return this;
-        }
-
-        public override void Enter(IInput input)
-        {
-            base.Enter(input);
-            _target = GetTarget(input);
-            //_target.y = 0;
-            var animator = Machine.gameObject.GetComponent<Animator>();
-            animator.SetBool("Moving", false);
-            animator.SetBool("Running", false);
-            Machine.StopAllCoroutines();
-            StartCoroutine(TurnTowards());
         }
 
         private Vector3 GetTarget(IInput input)
@@ -49,33 +44,7 @@ namespace ThreeK.Game.StateMachine.State
                 return (Vector3) input.Data;
             if (input.Data is Transform)
                 return ((Transform) input.Data).position;
-            return Machine.transform.position;
-        }
-
-        private IEnumerator TurnTowards()
-        {
-            var rigi = Machine.GetComponent<Rigidbody>();
-            var trans = Machine.transform;
-            var q = Quaternion.LookRotation(_target - rigi.position);
-            q.x = q.z = 0;
-            rigi.velocity = Vector3.zero;
-            var animator = Machine.gameObject.GetComponent<Animator>();
-            animator.SetBool("Moving", false);
-            animator.SetBool("Running", false);
-
-            while (true)
-            {
-                var r = Quaternion.RotateTowards(trans.rotation, q, _turnSpeed * Time.deltaTime);
-                trans.rotation = r;
-                var diff = Mathf.Abs(trans.eulerAngles.y - q.eulerAngles.y);
-                //Debug.Log(string.Format("{0}, {1}, {2}", diff, trans.rotation.eulerAngles.y, q.eulerAngles.y));
-                if (diff < 1)
-                {
-                    OnStateExit.Invoke();
-                    break;
-                }
-                yield return null;
-            }
+            return Vector3.zero;
         }
     }
 }
